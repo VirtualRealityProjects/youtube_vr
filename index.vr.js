6592794dbcfae84595132271bf3c30b6aeef0ff1
Sofiene Djebali <Sofiene Djebali>
2017-04-01 18:17:17 +0200

    Search button

--- index.vr.js
+++ index.vr.js
@@ -16,13 +16,20 @@ export default class demo_youtube extends React.Component {
 		this.state = {
 			isFocus: false,
 			isEnter: false,
-			text: ''
+			text: '',
+      isButtonEnter: false
 		}
 		this.handleChange = this.handleChange.bind(this);
+		this.search = this.search.bind(this);
 	}
 
 	handleChange(event) {
-		if (!event || event.eventType !== 'keydown' || !this.state.isFocus || (event.keyCode === 8 && this.state.text.trim() === '' )) {
+		if (!event || 
+      event.eventType !== 'keydown' || 
+      !this.state.isFocus || 
+      (event.keyCode === 8 && this.state.text.trim() === '' ) || 
+      (event.keyCode === 32 && this.state.text.trim() === '') ||
+      ((event.keyCode < 32 || event.keyCode > 126)) && event.keyCode !== 8) {
 			return;
 		}
 		this.setState({
@@ -30,51 +37,70 @@ export default class demo_youtube extends React.Component {
 		});
 	}
 
+  search() {
+    
+  }
+
   render() {
     return (
       <View>
         <Pano source={asset('chess-world.jpg')}/>
-        <View onInput={(e) => this.handleChange(e.nativeEvent.inputEvent)} style={{
+        <View 
+        onEnter={() => this.setState({ isEnter: true })}
+        onInput={(e) => this.handleChange(e.nativeEvent.inputEvent)} 
+        onExit={() => this.setState({ isEnter: false, isFocus: false })}
+        style={{
           width: 4,
-          backgroundColor: '#e74c3c',
           flexDirection: 'row',
-          paddingTop: 0.1,
-          paddingLeft: 0.1,
-          paddingRight: 0.1,
-          paddingBottom: 0.1,
           alignItems: 'stretch',
+          layoutOrigin: [0.5, 0.5],
+          marginTop: 0.1,
+          marginBottom: 0.1,
+          transform: [{ translate: [0, -0.2, -5] }]
         }}>
           <VrButton
             onInput={(e) => console.log(e)}
             onClick={() => this.setState({ isFocus: true })}
-            onEnter={() => this.setState({ isEnter: true })}
-            onExit={() => this.setState({ isEnter: false, isFocus: false })}
             style={{
-              flex: 1,
-              flexDirection: 'row',
-              alignItems: 'stretch',
-              justifyContent: 'space-around',
-              layoutOrigin: [0.5, 0.5],
-              transform: [{ translate: [0, -0.2, -5] }],
               width: 3,
+              alignItems: 'stretch',
               backgroundColor: '#ffffff',
               opacity: (this.state.isEnter) ? 1 : 0.5,
               borderWidth : 0.02,
-              borderColor: (this.state.isFocus) ? '#333333' : '#ffffff',
+              borderColor: (this.state.isFocus) ? '#34495e' : '#ffffff',
               paddingTop: 0.1,
               paddingLeft: 0.1,
               paddingRight: 0.1,
-              paddingBottom: 0.1,
-              marginTop: 0.1,
-              marginBottom: 0.1
+              paddingBottom: 0.1
             }}
           >
             <Text 
-              style={{ textAlign: 'left', color: 'black' }}
+              style={{ textAlign: 'left', opacity: (this.state.text.trim() === '') ? 0.7 : 1,  color: 'black' }}
             >
               {(this.state.text.trim() !== '') ? this.state.text : 'Search a VR video ...'}
             </Text>
           </VrButton>			
+          <VrButton
+            onEnter={() => this.setState({ isButtonEnter: true })}
+            onExit={() => this.setState({ isButtonEnter: false })}
+            onClick={this.search}
+            style={{
+              width: 1,
+              alignItems: 'stretch',
+              backgroundColor: (this.state.isButtonEnter) ? '#2c3e50' : '#34495e',
+              opacity: (this.state.isEnter) ? 1 : 0.5,
+              paddingTop: 0.1,
+              paddingLeft: 0.1,
+              paddingRight: 0.1,
+              paddingBottom: 0.1
+            }}
+          >
+            <Text 
+              style={{ textAlign: 'center', color: 'white' }}
+            >
+              SEARCH
+            </Text>
+          </VrButton>	
         </View>
       </View>
     );
